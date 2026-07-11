a9c974e4d1ec50ef7eb537f6a9587b34fdd33384
Lalit Mohan Agnihotri <lalitagnihotri62@gmail.com>
2026-07-11 19:38:57 +0530

    Time: 79 ms (10.36%), Space: 158.2 MB (10.4%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->b[0]-a[0]);
+        int ans[]=new int [nums.length-(k-1)];
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            pq.add(new int[]{nums[i],i});
+            while(pq.peek()[1]<=i-k){
+                pq.poll();
+            }
+            if(i>=k-1){
+                ans[j++]=pq.peek()[0];
+            }
+        }return ans;
+
+    }
+}
