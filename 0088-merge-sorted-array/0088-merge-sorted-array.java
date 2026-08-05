8117c266c2ed18d7185afc622a3262f0c5b15169
Lalit Mohan Agnihotri <lalitagnihotri62@gmail.com>
2026-08-05 17:49:09 +0530

    Time: 0 ms (100%), Space: 43.6 MB (78.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,15 +4,17 @@ class Solution {
         int b=n-1;
         int c=m+n-1;
         while(a>=0&&b>=0){
-            if(nums1[a]>nums2[b]){
-                nums1[c--]=nums1[a--];
-            }else{
-                nums1[c--]=nums2[b--];
+            if(nums1[a]<nums2[b]){
+                nums1[c]=nums2[b--];
+                c--;
+            }
+            else{
+                nums1[c]=nums1[a--];
+                c--;
             }
         }
         while(b>=0){
             nums1[c--]=nums2[b--];
         }
     }
-    
 }
