c4ca4040ce6c06b2eaab8f818a598eb9c8479b76
Lalit Mohan Agnihotri <lalitagnihotri62@gmail.com>
2026-08-11 21:59:28 +0530

    Time: 0 ms (100%), Space: 41.9 MB (70.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,12 +3,12 @@ class Solution {
         if(n<=1){
             return n;
         }
-        int curr=1;
-        int prev=0;
+        int dp[]=new int [n+1];
+        dp[0]=0;
+        dp[1]=1;
         for(int i=2;i<=n;i++){
-            int next=curr+prev;
-            prev=curr;
-            curr=next;
-        }return curr;
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
