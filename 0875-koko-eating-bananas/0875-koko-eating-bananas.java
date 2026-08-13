5fdbd55b4762de1e60746a891e87a6036e419da9
Lalit Mohan Agnihotri <lalitagnihotri62@gmail.com>
2026-08-13 23:53:53 +0530

    Time: 26 ms (14.61%), Space: 48.3 MB (16.43%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,11 +1,10 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int i=1;
-        int j=Integer.MIN_VALUE;
-        for(int a:piles){
-            j=Math.max(j,a);
+        int j=0;
+        for(int k:piles){
+            j=Math.max(k,j);
         }
-
         while(i<j){
             int mid=i+(j-i)/2;
             if(canEat(piles,mid,h)){
@@ -13,16 +12,13 @@ class Solution {
             }else{
                 i=mid+1;
             }
-
-        }return j;
+        }return i;
     }
-    public boolean canEat(int[]nums,int mid,int h){
-        int hour=0;
+    public boolean canEat(int nums[],int mid,int h){
+        int time=0;
         for(int i=0;i<nums.length;i++){
-            hour+=Math.ceil((double)nums[i]/mid);
-            if(hour>h){
-                return false;
-            }
+            time+=Math.ceil((double)nums[i]/mid);
+            if(time>h)return false;
         }return true;
     }
 }
