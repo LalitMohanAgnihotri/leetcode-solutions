61b8405fb347764319a74a863e548cd1493ac6bd
Lalit Mohan Agnihotri <lalitagnihotri62@gmail.com>
2026-08-13 21:58:25 +0530

    Time: 0 ms (100%), Space: 44.8 MB (48.65%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -7,7 +7,7 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            if(nums[mid]>target){
+            else if(nums[mid]>target){
                 j=mid-1;
             }else{
                 i=mid+1;
