62d14439161417c35191855aa61683abdcc8c596
Lalit Mohan Agnihotri <lalitagnihotri62@gmail.com>
2026-08-21 22:01:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int[] plusOne(int[] digits) {
-        int c=0;
-        for(int i=digits.length-1;i>=0;i--){
-            if(digits[i]==9){
-                digits[i]=0;
-                c++;
-            }
-            else{
-                digits[i]=digits[i]+1;
+    public int[] plusOne(int[] d) {
+        int nine=0;
+        for(int i=d.length-1;i>=0;i--){
+            if(d[i]==9){
+                d[i]=0;
+                nine++;
+            }else{
+                d[i]=d[i]+1;
                 break;
             }
-            
         }
-        if(c==digits.length){
-            int ans[]=new int[digits.length+1];
-           ans[0]=1;
-           return ans;
-        }return digits;
+        if(nine==d.length){
+            int ans[]=new int[d.length+1];
+            ans[d.length-1]=1;
+            return ans;
+        }
+        else return d;
     }
 }
